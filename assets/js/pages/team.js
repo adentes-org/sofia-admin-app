2ac07c809044f2fc0e36e8d4574ecbdb7b9caeb4
Antoine GIRARD <sapk@users.noreply.github.com>
2016-07-06 15:46:17 +0200

    Update team.js

--- assets/js/pages/team.js
+++ assets/js/pages/team.js
@@ -2,28 +2,41 @@ define(["jquery"], function($) {
   return {
     props: ['db'],
     data: function () {
-  	return {users : {}};
+  	return {users : []};
     },
     template: '<h2>Team Management</h2>'+
               '<table>'+
                 '<thead><tr><th>Name</th><th>Password</th><th>Roles</th><th>Action</th></tr></thead>'+
                 '<tbody>'+
                 	'<tr id="add-user"><td><input type="text" id="name" placeholder="username" style="width: 240px;"></td><td><input type="text" id="password" placeholder="password" style="width: 240px;"></td><td></td><td><button class="button-primary" @click="addUser;">Ajouter</button></td></tr>'+
-                	'<tr v-for="(id, user) in users" data-name="{{user.name}}" data-id="{{user._id}}" data-rev="{{user._rev}}"><td>{{user.name}}</td><td><button class="button button-outline" @click="resetPass">Reset</button></td><td>{{user.roles|json}}</td><td><button class="button button-outline" @click="delUser">Delete</button></td></tr>'+
+                	'<tr v-for="(i, user) in users" data-name="{{user.name}}" data-id="{{user._id}}" data-rev="{{user._rev}}"><td>{{user.name}}</td><td><button class="button button-outline" @click="resetPass">Reset</button></td><td>{{user.roles|json}}</td><td><button class="button button-outline" @click="delUser">Delete</button></td></tr>'+
                 '</tbody>'+
               '</table>',
     methods:{
-    	resetPass: function(event){
-    		
-    	},
-  	addUser : function(event){
-  	},
-  	delUser : function(event){
-  		
-  	}
+      getUsers : function(){
+        var vue = this;
+                    //Filling user table
+        this.db.users.allDocs({ include_docs: true }).then(function (result) {
+          console.log("User list",result);
+          $.each(result.rows, function (index, value) {
+            var user = value.doc;
+            if (user.type != 'user')
+              return; //Ex _design doc
+            console.log(user);
+            vue.users.push(user);
+          });
+        }).catch(function (err) {
+          //TODO handle err
+          console.log(err);
+        });
+      }
+    	resetPass: function(event){},
+  	  addUser : function(event){},
+  	  delUser : function(event){}
     },
     events: {
   	onload : function(){
+  	  this.getUsers();
   	},
   	onchange : function(change){
   	}
