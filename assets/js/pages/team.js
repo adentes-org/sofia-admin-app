365b792701c3ff1356bb060cdab5b0d607d70b34
Antoine GIRARD <sapk@users.noreply.github.com>
2016-07-06 15:41:34 +0200

    Update team.js

--- assets/js/pages/team.js
+++ assets/js/pages/team.js
@@ -9,7 +9,7 @@ define(["jquery"], function($) {
                 '<thead><tr><th>Name</th><th>Password</th><th>Roles</th><th>Action</th></tr></thead>'+
                 '<tbody>'+
                 	'<tr id="add-user"><td><input type="text" id="name" placeholder="username" style="width: 240px;"></td><td><input type="text" id="password" placeholder="password" style="width: 240px;"></td><td></td><td><button class="button-primary" @click="addUser;">Ajouter</button></td></tr>'+
-                	'<tr v-for="(id, user) in users" data-name="{{user.name}}" data-id="{{user._id}}" data-rev="{{user._rev}}"><td>{{user.name}}</td><td><button class="button button-outline" @click="resetPass">Reset</button></td><td>{{user.roles|json}}</td><td><button class="button button-outline" @click="delUser">Delete</button></td></tr>'
+                	'<tr v-for="(id, user) in users" data-name="{{user.name}}" data-id="{{user._id}}" data-rev="{{user._rev}}"><td>{{user.name}}</td><td><button class="button button-outline" @click="resetPass">Reset</button></td><td>{{user.roles|json}}</td><td><button class="button button-outline" @click="delUser">Delete</button></td></tr>'+
                 '</tbody>'+
               '</table>',
     methods:{
