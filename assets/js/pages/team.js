ff95eb7e6a4f6e513cd656f2a582a9a2f0cd0dd2
Antoine GIRARD <sapk@users.noreply.github.com>
2016-07-06 15:40:22 +0200

    Update team.js

--- assets/js/pages/team.js
+++ assets/js/pages/team.js
@@ -1,23 +1,32 @@
 define(["jquery"], function($) {
   return {
     props: ['db'],
-  	data: function () {
-  	        return {};
+    data: function () {
+  	return {users : {}};
     },
     template: '<h2>Team Management</h2>'+
               '<table>'+
                 '<thead><tr><th>Name</th><th>Password</th><th>Roles</th><th>Action</th></tr></thead>'+
-                '<tbody><tr id="add-user"><td><input type="text" id="name" placeholder="username" style="width: 240px;"></td><td><input type="text" id="password" placeholder="password" style="width: 240px;"></td><td></td><td><button class="button-primary" onclick="addUser;">Ajouter</button></td></tr></tbody>'+
+                '<tbody>'+
+                	'<tr id="add-user"><td><input type="text" id="name" placeholder="username" style="width: 240px;"></td><td><input type="text" id="password" placeholder="password" style="width: 240px;"></td><td></td><td><button class="button-primary" @click="addUser;">Ajouter</button></td></tr>'+
+                	'<tr v-for="(id, user) in users" data-name="{{user.name}}" data-id="{{user._id}}" data-rev="{{user._rev}}"><td>{{user.name}}</td><td><button class="button button-outline" @click="resetPass">Reset</button></td><td>{{user.roles|json}}</td><td><button class="button button-outline" @click="delUser">Delete</button></td></tr>'
+                '</tbody>'+
               '</table>',
-  	methods:{
-  	        addUser : function(){
-  	        }
+    methods:{
+    	resetPass: function(event){
+    		
+    	},
+  	addUser : function(event){
   	},
-  	events: {
-  	        onload : function(){
-  	        },
-  	        onchange : function(change){
-  	        }
-        }
-	};
+  	delUser : function(event){
+  		
+  	}
+    },
+    events: {
+  	onload : function(){
+  	},
+  	onchange : function(change){
+  	}
+     }
+  };
 })
