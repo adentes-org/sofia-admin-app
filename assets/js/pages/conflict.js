f698b22e2f6f39e464a544884fcf602f3214d560
Antoine GIRARD <sapk@sapk.fr>
2016-07-08 02:01:54 +0200

    Complete backport

--- assets/js/pages/conflict.js
+++ assets/js/pages/conflict.js
@@ -1,12 +1,210 @@
-define([], function() {
+define(["objectdiff","jquery"], function(objectDiff,$) {
   return {
-  		props: [],
+  		props: ["db"],
 			data: function () {
-    		return { }
+    		return {
+          conflicts : {},
+          current : {
+            src : {},
+            conflict : {},
+            result : "",
+            diff : ""
+          }
+        }
   		},
-  		template: '<h2>Conflict</h2>'+
-          '',
+  		template: '<button v-show="mergeButton" class="button-primary float-right" @click="validateMerge">Validate Merge</button>'+
+                '<h2>Conflict</h2>'+
+	              '<div id="editor-header" class="row" style="width: calc(100% - 2.2em);">'+
+                  '<div class="column column-33">Source</div>'+
+                  '<div class="column column-33">Conflict</div>'+
+                  '<div class="column column-33">Result (editable) <div class="float-right"><button class="button button-small" @click="showRaw" disabled="disabled">raw</button> / <button @click="showDiff" class="button button-small">diff</button></div></div>'+
+                '</div>'+
+          	    '<div id="editor" class="row" style="width: calc(100% - 2.2em);">'+
+                  '<div id="src" class="column column-33">{{current.src | json}}</div>'+
+                  '<div id="conflict" class="column column-33">{{current.conflict | json}}</div>'+
+                  '<div id="result" class="column column-33">'+
+                    '<textarea class="raw" v-model="current.result"></textarea>'+
+                    '<pre class="diff-box" style="display:none">{{{current.diff}}}</pre>'+
+                  '</div>'+
+                '</div>'+
+                '<hr>'+
+	              '<div id="conflicts">'+
+                  '<div v-for="(id, cflics) in conflicts" class="fiche" id="fiche-{{id}}" style="width : 33%" >'+
+                    '{{id}}<p><a class="button button-small" @click="resolveConflict" data-fiche="{{id}}" data-conflict="{{conflict}}" v-for="conflict in cflics">{{conflict}}</a></p>'+
+                  '</div>'+
+                '</div>',
+      computed : {
+        mergeButton : function(){
+          try {
+    		    var resultObj = JSON.parse(this.current.result);
+            if(typeof resultObj._id === "undefined" || typeof resultObj._rev === "undefined"){
+              return false
+            }
+          } catch (e) {
+            return false
+          }
+          return true
+        }
+      },
 			methods:{
-			}
+        getConflicts : function(){
+          var vue = this;
+          var conflicts = {};
+          return this.db.fiches.allDocs({
+            include_docs: true,
+            conflicts: true,
+            attachments: true
+          }).then(function(result){
+            console.log(result);
+            $.each(result.rows, function (index, obj) {
+              //console.log(obj.doc)
+              if(typeof obj.doc["_conflicts"] !== "undefined" && obj.doc["_conflicts"].length > 0 ){
+                //We got conflict
+                console.log("Conflict !" , obj.doc);
+                conflicts[obj.doc._id] = obj.doc._conflicts;
+              }
+            });
+            vue.conflicts = conflicts;
+            //TODO check if the current conflict is still present
+          }).catch(function (err) {
+              console.log(err);
+          });
+        },
+        resolveConflict :  function(event){
+          var vue = this;
+          var el = $(event.target);
+          var fiche = el.attr("data-fiche");
+          var conflict = el.attr("data-conflict");
+          console.log(el,fiche,conflict);
+          console.log("conflict getting doc:",fiche,conflict);
+          vue.db.fiches.get(fiche).then(function(doc) {
+            vue.db.fiches.get(fiche, {rev: conflict}).then(function (conflict) {
+              // do something with the doc
+                console.log("conflict resolution:",doc,conflict);
+                vue.current.src=doc;
+                vue.current.conflict=conflict;
+                if(doc.events[doc.events.length-1].timestamp>conflict.events[conflict.events.length-1].timestamp){
+                  vue.current.result=JSON.stringify(vue.mergeConflict(conflict,doc,doc._rev)); //The doc in DB is more recent that the conflict
+                }else {
+                  vue.current.result=JSON.stringify(vue.mergeConflict(doc,conflict,doc._rev)); //The conflict in DB is more recent that the conflict
+                }
+                vue.showRaw();
+                /*
+                $('#conflict>button').removeAttr('disabled').attr("data-id",id).attr("data-rev",rev)
+                */
+            }).catch(function (err) {
+              // handle any errors
+              alert(err.message)
+            });
+          }).catch(function (err) {
+            // handle any errors
+            alert(err.message)
+          });
+
+        },
+        mergeConflict : function(o,n,rev){ // o : obj in db, n: obj to commit
+           /* This will merge and keep a maximum of information (things deleted previously could be added) */
+           var ret = $.extend({},o,{
+             close_context : n.close_context,
+             deleted : n.deleted,
+             closed : n.closed,
+             patient : n.patient,
+             origin : n.origin,
+             owner_id : n.owner_id,
+             primaryAffection : n.primaryAffection,
+             uid : n.uid
+           }); //Close and overwrite some parts that can be directly
+           $.each(n.pathologys, function(id,val){
+             if ($.inArray(val,ret.pathologys) === -1) { //Not found
+               ret.pathologys.push(val);
+             }
+           });
+           $.each(n.events, function(id,val){
+               var found = false;
+               var searching = JSON.stringify(val);
+               $.each(ret.events, function(i,v){
+                 if(!found && JSON.stringify(v) === searching ){
+                   found = true;
+                 }
+               });
+               if(!found){
+                 ret.events.push(val);
+               }
+           });
+           ret.events.sort(function(x, y){ //Order
+             return x.timestamp - y.timestamp;
+           });
+           ret._rev=rev;
+           return ret;
+        },
+        getDiffConflict : function(o,n){
+          return objectDiff.diff($.extend({}, o, {events : []}), $.extend({}, n, {events : []})) //Create a shallow clone and overwrite events parts to not be big in term of data
+        },
+        validateMerge : function(){
+          var vue = this;
+          $('#conflict>button').attr('disabled', 'disabled').text('Sending ...').blur();
+          var obj = JSON.parse(vue.current.result);
+          obj.events.push({
+                    type : "action",
+                    action : "AdminMergeConflict",
+                    message : "Conflict detected and merged by admin!",
+                    /* This take to much space
+                    diff : objectDiff.diff(JSON.parse($(".page#conflict>#editor>#src").html()),obj),
+                    conflict : JSON.parse($(".page#conflict>#editor>#conflict").html()),
+                    */
+                    diff: vue.getDiffConflict(vue.current.src,obj),
+                    timestamp : Date.now(),
+                    user :  vue.db.users.__opts.auth.username
+          });
+          vue.db.fiches.put(obj).then(function () {
+            $('#conflict>button').text('Removing conflict ...').blur();
+            vue.db.fiches.remove(vue.current.conflict._id, vue.current.conflict._rev).then(function () {
+              $('#conflict>button').removeAttr('disabled').text('Finish !').css('background-color', 'green');
+              //getConflicts(); //update conflict list done by monitoring changes
+              window.setTimeout('$("#conflict>button").text("Validate Merge").css("background-color", "#9b4dca")', 1000);
+              vue.current.src={};vue.current.conflict={};
+              vue.current.result="";vue.current.diff="";
+              vue.showRaw();
+              // yay, we're done
+            }).catch(function (err) {
+                // handle any errors
+                console.log(err);
+                alert(err.message);
+            });
+          }).catch(function (err) {
+            console.log(err);
+            alert(err.message);
+          });
+        },
+        showRaw : function(){
+            console.log("Showing raw")
+            $("button:contains('raw')").attr('disabled', 'disabled');
+            $("button:contains('diff')").removeAttr('disabled');
+            $(".page#conflict>#editor>#result>.diff-box").hide()
+            $(".page#conflict>#editor>#result>.raw").show()
+            window.setTimeout(function(){
+              $(".page#conflict>#editor>#result>.raw")[0].style.height = "5px";
+              $(".page#conflict>#editor>#result>.raw")[0].style.height = ($(".page#conflict>#editor>#result>.raw")[0].scrollHeight+5)+"px";
+            },100);
+        },
+        showDiff : function(){
+            console.log("Showing diff")
+            var diff = objectDiff.diffOwnProperties(this.current.src,JSON.parse(this.current.result));
+            console.log(diff);
+            this.current.diff = objectDiff.convertToXMLString(diff)
+            $("button:contains('raw')").removeAttr('disabled');
+            $("button:contains('diff')").attr('disabled', 'disabled');
+            $(".page#conflict>#editor>#result>.diff-box").show()
+            $(".page#conflict>#editor>#result>.raw").hide()
+        }
+			},
+    	events: {
+    	  	  onload : function(){
+    	  	    this.getConflicts();
+    	  	  },
+    	  	  onchange : function(change){
+    	  	    this.getConflicts();
+    	  	  }
+    	}
 	};
 })
--- assets/lib/objectdiff/objectDiff.js
+++ assets/lib/objectdiff/objectDiff.js
@@ -3,7 +3,6 @@ var objectDiff = typeof exports != 'undefined' ? exports : {};
 /**
  * @param {Object} a
  * @param {Object} b
- * @nosideeffects
  * @return {Object}
  */
 objectDiff.diff = function diff(a, b) {
@@ -15,25 +14,33 @@ objectDiff.diff = function diff(a, b) {
 		}
 	}
 
-	var result = {};
+	var value = {};
+	var equal = true;
 
 	for (var key in a) {
 		if (key in b) {
 			if (a[key] === b[key]) {
-				result[key] = {
-					changed: 'equals',
+				value[key] = {
+					changed: 'equal',
 					value: a[key]
 				}
 			} else {
 				var typeA = typeof a[key];
 				var typeB = typeof b[key];
-				if ((typeA == 'object' || typeA == 'function') && (typeB == 'object' || typeB == 'function')) {
-					result[key] = {
-						changed: 'object change',
-						diff: diff(a[key], b[key])
+				if (a[key] && b[key] && (typeA == 'object' || typeA == 'function') && (typeB == 'object' || typeB == 'function')) {
+					var valueDiff = diff(a[key], b[key]);
+					if (valueDiff.changed == 'equal') {
+						value[key] = {
+							changed: 'equal',
+							value: a[key]
+						}
+					} else {
+						equal = false;
+						value[key] = valueDiff;
 					}
 				} else {
-					result[key] = {
+					equal = false;
+					value[key] = {
 						changed: 'primitive change',
 						removed: a[key],
 						added: b[key]
@@ -41,7 +48,8 @@ objectDiff.diff = function diff(a, b) {
 				}
 			}
 		} else {
-			result[key] = {
+			equal = false;
+			value[key] = {
 				changed: 'removed',
 				value: a[key]
 			}
@@ -50,21 +58,31 @@ objectDiff.diff = function diff(a, b) {
 
 	for (key in b) {
 		if (!(key in a)) {
-			result[key] = {
+			equal = false;
+			value[key] = {
 				changed: 'added',
 				value: b[key]
 			}
 		}
 	}
 
-	return result;
+	if (equal) {
+		return {
+			changed: 'equal',
+			value: a
+		}
+	} else {
+		return {
+			changed: 'object change',
+			value: value
+		}
+	}
 };
 
 
 /**
  * @param {Object} a
  * @param {Object} b
- * @nosideeffects
  * @return {Object}
  */
 objectDiff.diffOwnProperties = function diffOwnProperties(a, b) {
@@ -76,27 +94,35 @@ objectDiff.diffOwnProperties = function diffOwnProperties(a, b) {
 		}
 	}
 
-	var result = {};
+	var diff = {};
+	var equal = true;
 	var keys = Object.keys(a);
 
 	for (var i = 0, length = keys.length; i < length; i++) {
 		var key = keys[i];
 		if (b.hasOwnProperty(key)) {
 			if (a[key] === b[key]) {
-				result[key] = {
-					changed: 'equals',
+				diff[key] = {
+					changed: 'equal',
 					value: a[key]
 				}
 			} else {
 				var typeA = typeof a[key];
 				var typeB = typeof b[key];
-				if ((typeA == 'object' || typeA == 'function') && (typeB == 'object' || typeB == 'function')) {
-					result[key] = {
-						changed: 'object change',
-						diff: diffOwnProperties(a[key], b[key])
+				if (a[key] && b[key] && (typeA == 'object' || typeA == 'function') && (typeB == 'object' || typeB == 'function')) {
+					var valueDiff = diffOwnProperties(a[key], b[key]);
+					if (valueDiff.changed == 'equal') {
+						diff[key] = {
+							changed: 'equal',
+							value: a[key]
+						}
+					} else {
+						equal = false;
+						diff[key] = valueDiff;
 					}
 				} else {
-					result[key] = {
+					equal = false;
+					diff[key] = {
 						changed: 'primitive change',
 						removed: a[key],
 						added: b[key]
@@ -104,7 +130,8 @@ objectDiff.diffOwnProperties = function diffOwnProperties(a, b) {
 				}
 			}
 		} else {
-			result[key] = {
+			equal = false;
+			diff[key] = {
 				changed: 'removed',
 				value: a[key]
 			}
@@ -116,93 +143,79 @@ objectDiff.diffOwnProperties = function diffOwnProperties(a, b) {
 	for (i = 0, length = keys.length; i < length; i++) {
 		key = keys[i];
 		if (!a.hasOwnProperty(key)) {
-			result[key] = {
+			equal = false;
+			diff[key] = {
 				changed: 'added',
 				value: b[key]
 			}
 		}
 	}
 
-	return result;
+	if (equal) {
+		return {
+			value: a,
+			changed: 'equal'
+		}
+	} else {
+		return {
+			changed: 'object change',
+			value: diff
+		}
+	}
 };
 
 
 (function() {
 
 	/**
-	 * @param {Object} diff
+	 * @param {Object} changes
 	 * @return {string}
 	 */
-	objectDiff.convertToXMLString = function convertToXMLString(diff) {
-		var output = '';
+	objectDiff.convertToXMLString = function convertToXMLString(changes) {
 		var properties = [];
+
+		var diff = changes.value;
+		if (changes.changed == 'equal') {
+			return inspect(diff);
+		}
 
 		for (var key in diff) {
 			var changed = diff[key].changed;
 			switch (changed) {
-				case 'equals':
+				case 'equal':
 					properties.push(stringifyObjectKey(escapeHTML(key)) + '<span>: </span>' + inspect(diff[key].value));
 					break;
 
 				case 'removed':
-					properties.push('<del>' + stringifyObjectKey(escapeHTML(key)) + '<span>: </span>' + inspect(diff[key].value) + '</del>');
+					properties.push('<del class="diff">' + stringifyObjectKey(escapeHTML(key)) + '<span>: </span>' + inspect(diff[key].value) + '</del>');
 					break;
 
 				case 'added':
-					properties.push('<ins>' + stringifyObjectKey(escapeHTML(key)) + '<span>: </span>' + inspect(diff[key].value) + '</ins>');
+					properties.push('<ins class="diff">' + stringifyObjectKey(escapeHTML(key)) + '<span>: </span>' + inspect(diff[key].value) + '</ins>');
 					break;
 
 				case 'primitive change':
 					var prefix = stringifyObjectKey(escapeHTML(key)) + '<span>: </span>';
 					properties.push(
-						'<del class="key">' + prefix + escapeHTML(inspect(diff[key].removed)) + '</del><span>,</span>\n' +
-						'<ins class="key">' + prefix + escapeHTML(inspect(diff[key].added)) + '</ins>');
+						'<del class="diff diff-key">' + prefix + inspect(diff[key].removed) + '</del><span>,</span>\n' +
+						'<ins class="diff diff-key">' + prefix + inspect(diff[key].added) + '</ins>');
 					break;
 
 				case 'object change':
-					properties.push(stringifyObjectKey(key) + '<span>: </span>' + convertToXMLString(diff[key].diff));
+					properties.push(stringifyObjectKey(key) + '<span>: </span>' + convertToXMLString(diff[key]));
 					break;
 			}
 		}
 
-		return '<span>{</span>\n<div class="level">' + properties.join('<span>,</span>\n') + '\n</div><span>}</span>';
+		return '<span>{</span>\n<div class="diff-level">' + properties.join('<span>,</span>\n') + '\n</div><span>}</span>';
 	};
-
-
-	var _repeatCache = {};
-	/**
-	 * repeatString("\t", 2) ==> "\t\t"
-	 * @param {String} string
-	 * @param {Number} times
-	 */
-	function repeatString(string, times) {
-		if (times == 1) {
-			return string;
-		} else if (times < 1) {
-			return '';
-		}
-
-		var key = string + times;
-
-		if (_repeatCache[key]) {
-			return _repeatCache[key];
-		} else {
-			var result = string;
-			for (var i = times; --i;) {
-				result += string;
-			}
-			_repeatCache[key] = result;
-		}
-
-		return result;
-	}
 
 	/**
 	 * @param {string} key
 	 * @return {string}
 	 */
 	function stringifyObjectKey(key) {
-		return /^[a-z_$][a-z0-9_$]*$/i.test(key) ?
+		return /^[a-z0-9_$]*$/i.test(key) ?
 			key :
 			JSON.stringify(key);
 	}
@@ -220,19 +233,52 @@ objectDiff.diffOwnProperties = function diffOwnProperties(a, b) {
 	 * @return {string}
 	 */
 	function inspect(obj) {
-		switch(typeof obj) {
-			case 'object':
-				var properties = [];
-				for (var key in obj) {
-					properties.push(stringifyObjectKey(escapeHTML(key)) + '<span>: </span>' + inspect(obj[key]));
-				}
-				return '<span>{</span>\n<div class="level">' + properties.join('<span>,</span>\n') + '\n</div><span>}</span>';
 
-			case 'string':
-				return JSON.stringify(escapeHTML(obj));
+		return _inspect('', obj);
 
-			default:
-				return escapeHTML(obj.toString());
+		/**
+		 * @param {string} accumulator
+		 * @param {object} obj
+		 * @see http://jsperf.com/continuation-passing-style/3
+		 * @return {string}
+		 */
+		function _inspect(accumulator, obj) {
+			switch(typeof obj) {
+				case 'object':
+					if (!obj) {
+						accumulator += 'null';
+						break;
+					}
+					var keys = Object.keys(obj);
+					var length = keys.length;
+					if (length === 0) {
+						accumulator += '<span>{}</span>';
+					} else {
+						accumulator += '<span>{</span>\n<div class="diff-level">';
+						for (var i = 0; i < length; i++) {
+							var key = keys[i];
+							accumulator = _inspect(accumulator + stringifyObjectKey(escapeHTML(key)) + '<span>: </span>', obj[key]);
+							if (i < length - 1) {
+								accumulator += '<span>,</span>\n';
+							}
+						}
+						accumulator += '\n</div><span>}</span>'
+					}
+					break;
+
+				case 'string':
+					accumulator += JSON.stringify(escapeHTML(obj));
+					break;
+
+				case 'undefined':
+					accumulator += 'undefined';
+					break;
+
+				default:
+					accumulator += escapeHTML(String(obj));
+					break;
+			}
+			return accumulator;
 		}
 	}
 })();
