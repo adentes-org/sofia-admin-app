250a3f6027d6d3ab11201586a7c76e4fba8b8343
Antoine GIRARD <sapk@users.noreply.github.com>
2016-07-15 16:28:34 +0200

    Add jsPDF lib to init config

--- assets/js/init.js
+++ assets/js/init.js
@@ -2,22 +2,23 @@ requirejs.config({
     baseUrl: "assets",
     paths: {
       app: "js",
-	    lib: "lib",
+      lib: "lib",
 
-	    main: "js/main",
+      main: "js/main",
 
       jquery: 'lib/jquery/jquery-3.0.0.min',
       //jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min',
       trumbowyg: 'lib/trumbowyg/trumbowyg.min',
 
       vue: 'lib/vuejs/vue.min',
-	    pouchdb: 'lib/pouchdb/pouchdb-5.4.4.min',
+      pouchdb: 'lib/pouchdb/pouchdb-5.4.4.min',
 
-	    qrcode: 'lib/qrcode/qrcode.min',
-	    objectdiff: 'lib/objectdiff/objectDiff',
-	    highcharts: 'lib/highcharts/highcharts',
-	    "highcharts-more": 'lib/highcharts/highcharts-more',
-	    "highcharts-solid-gauge": 'lib/highcharts/modules/solid-gauge'
+      qrcode: 'lib/qrcode/qrcode.min',
+      jspdf: 'lib/jspdf/jspdf.min',
+      objectdiff: 'lib/objectdiff/objectDiff',
+      highcharts: 'lib/highcharts/highcharts',
+      "highcharts-more": 'lib/highcharts/highcharts-more',
+      "highcharts-solid-gauge": 'lib/highcharts/modules/solid-gauge'
     },
     shim: {
         "main": {
@@ -35,6 +36,9 @@ requirejs.config({
         },
         qrcode: {
             exports: 'QRCode'
+        },
+        jspdf: {
+            exports: 'jsPDF'
         },
         objectdiff: {
             exports: 'objectDiff'
