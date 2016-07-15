e1145685fa75299ebc0a9bf2dec00bbea6ff5780
Antoine GIRARD <sapk@sapk.fr>
2016-07-15 19:43:05 +0200

    Fix #1 by generating PDF in js

--- assets/js/pages/export.js
+++ assets/js/pages/export.js
@@ -35,37 +35,46 @@ define(["jquery","qrcode","jspdf","app/tool"], function($,QRCode,jsPDF,tool) {
 			var larg = 100
 			var haut = 40
 			var marg = (210-larg*2)/3;
-			
+
 			console.log("Collecting data ..."); //TODO maybe
 			console.log("Generating PDF");
 			var doc = new jsPDF();
 			var position = {x:0,y:0};
 			$.each(this.users, function (i, user) {
-				position.x = (i%2==1)?marg*2+larg:marg; 
+				position.x = (i%2==1)?marg*2+larg:marg;
 				position.y += (i%2==1)?0:marg;
-				
-				
-				doc.rect(position.x, position.y, larg, haut); 
-				doc.rect(position.x+larg-haut, position.y, haut, haut); 
-				
-				doc.setFontSize(12);
-				doc.text(position.x+marg, position.y+marg*2, 'URL :');
-				doc.text(position.x+marg, position.y+marg*2+3*haut/8, "Database Name : "); 
-        			//TODO align dbname text to right (same for the others) 
-				doc.text(position.x+marg, position.y+marg*2+4*haut/8, vue.db.config.dbname.fiche);
-				doc.text(position.x+marg, position.y+marg*2+5*haut/8, "Pseudo : "+user.name); 
+
+
+				doc.rect(position.x, position.y, larg, haut);
+				doc.rect(position.x+larg-haut, position.y, haut, haut);
+
+
+        doc.setFontSize(11).setTextColor(150).text(position.x+marg   , position.y+marg*2, "Pseudo");
+        doc.setFontSize(11).setTextColor(150).text(position.x+marg+20, position.y+marg*2, ":");
+        doc.setFontSize(11).setTextColor(0).text(position.x+marg+23  , position.y+marg*2, user.name);
 				if(vue.passwords[user._id]){
-					doc.text(position.x+marg, position.y+marg*2+6*haut/8, "Password : "+vue.passwords[user._id]);
-        			}
-        			
-        			url = doc.setFontSize(11).splitTextToSize(vue.db.config.url, larg-haut-marg*2)
-				doc.text(position.x+marg, position.y+marg*2+haut/8, url);
-				
+					doc.setFontSize(11).setTextColor(150).text(position.x+marg   , position.y+marg*2+1*5, "Password");
+          doc.setFontSize(11).setTextColor(150).text(position.x+marg+20, position.y+marg*2+1*5, ":");
+          doc.setFontSize(11).setTextColor(0).text(position.x+marg+23  , position.y+marg*2+1*5, vue.passwords[user._id]);
+        }
+
+        doc.setFontSize(10).setTextColor(150).text(position.x+marg, position.y+marg*2+2*5+2, "Database Name : ");
+				doc.setFontSize(11).setTextColor(0).text(position.x+marg, position.y+marg*2+3*5+2, vue.db.config.dbname.fiche);
+				doc.setFontSize(10).setTextColor(150).text(position.x+marg, position.y+marg*2+4*5+2, 'URL :');
+        //TODO align dbname text to right (same for the others)
+
+        url = doc.setFontSize(10).splitTextToSize(vue.db.config.url, larg-haut-marg*2)
+				doc.setTextColor(0).text(position.x+marg, position.y+marg*2+5*5+1, url);
+
 				var elQRCode = $("#qrcode-user-"+i);
 				imgData = elQRCode.find("img[src^='data']").attr('src'); //data:image/png;base64
-				doc.addImage(imgData, 'PNG', position.x+larg-haut, position.y, haut, haut);
-				
-				position.y += (i%2==1)?haut:0; //Saut de ligne
+				doc.addImage(imgData, 'PNG', position.x+larg-haut+0.2, position.y+0.2, haut-0.4, haut-0.4);
+
+				position.y += (i%2==1)?haut:0; //New line
+        if(position.y+haut+marg>297){ //New page
+    			position = {x:0,y:0};
+          doc.addPage();
+        }
 			});
 			console.log("Going PDF view");
 			doc.save();
