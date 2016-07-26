e734dcb975d716c1fa85d6bafc891455a297ddbd
Antoine GIRARD <sapk@users.noreply.github.com>
2016-07-26 15:17:56 +0200

    Update db.js

--- assets/js/pages/db.js
+++ assets/js/pages/db.js
@@ -1,47 +1,69 @@
+/* global define */
+
 define(["jquery"], function($) {
-  return {
-  		props: ['db'],
-			data: function () {
-    		return {
-    		  fiches : {}
-    		}
-  		},
-  		template: '<h2>Database</h2>'+	              
-                '<div id="fiches">'+
-                  '<div v-for="(id, fiche) in fiches" class="fiche" id="fiche-{{id}}" style="width : 33%" >'+
-                    '{{id}}'+
-                  '</div>'+
-                '</div>',
-			methods:{
-			  getFiches : function(){
-			    var vue = this;
-          var fiches = {};
-          return this.db.fiches.allDocs({
-            include_docs: true,
-            conflicts: true,
-            attachments: false
-          }).then(function(result){
-            console.log(result);
-            $.each(result.rows, function (index, obj) {
-              //console.log(obj.doc)
-              if(obj.doc._id.startsWith("_")){
-              	return //Skipping config file like "_design/sofia-config"
-              }
-              fiches[obj.doc._id] = obj.doc;
-            });
-            vue.fiches = fiches;
-          }).catch(function (err) {
-              console.log(err);
-          });
-			  }
-			},
-			events: {
-        onload : function(){
-    	  	    this.getFiches();
+    return {
+        props: ['db'],
+        data: function() {
+            return {
+                fiches: {},
+                tests: {
+                	viewExistence : {
+                		description : "Les indexes sont présent ?",
+                		validator : function(db){
+                			this.result = true;
+                		},
+                		result : false
+                	}
+                }
+            };
         },
-        onchange : function(){
-    	  	    this.getFiches();
+        template:   '<h2>Database</h2>' +
+	            '<div id="fiches">' +
+		            '<div v-for="(id, fiche) in fiches" class="fiche" id="fiche-{{id}}" style="width : 33%" >' +
+		            	'{{id}}' +
+		            '</div>' +
+	            '</div>' +
+	            '<div id="tests">' +
+		            '<div v-for="(id, test) in tests" class="tes" id="test-{{id}}">' +
+		            	'{{test.description}} : {{test.result}}' +
+		            '</div>' +
+	            '</div>',
+        methods: {
+            testViews: function() {
+            	var vue = this;
+                $.each(vue.tests, function(index, test) {
+                	test.validator.call(test, vue.db); //Call in context of test
+                });
+            },
+            getFiches: function() {
+                var vue = this;
+                var fiches = {};
+                return this.db.fiches.allDocs({
+                    include_docs: true,
+                    conflicts: true,
+                    attachments: false
+                }).then(function(result) {
+                    console.log(result);
+                    $.each(result.rows, function(index, obj) {
+                        //console.log(obj.doc)
+                        if (obj.doc._id.startsWith("_")) {
+                            return; //Skipping config file like "_design/sofia-config"
+                        }
+                        fiches[obj.doc._id] = obj.doc;
+                    });
+                    vue.fiches = fiches;
+                }).catch(function(err) {
+                    console.log(err);
+                });
+            }
+        },
+        events: {
+            onload: function() {
+                this.getFiches().then(this.testViews);
+            },
+            onchange: function() {
+                this.getFiches().then(this.testViews);
+            }
         }
-      }
-	};
-})
+    };
+});
